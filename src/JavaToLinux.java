79162b4c9a4033fcbb070d74f4edbdf03f0d2dc4
舞枫 <kingwufeng@wufengdeMacBook-Pro.local>
2018-01-31 01:07:48 +0800

    4

--- src/JavaToLinux.java
+++ src/JavaToLinux.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -6,14 +7,30 @@ public class JavaToLinux {
 
     public static void main(String[] args) {
         Process process = null;
-        String[] cmds = {"/bin/bash", "-c", "cat LinuxChannel.iml"};
-        //Scanner sc = null;
+        String[] cmds = {"/bin/bash", "-c", "cat "};
+
+        String filePath = "/Users/kingwufeng/workspace/LinuxChannel/data/";
+        File file = new File(filePath);
+        String[] fileNames = file.list();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入sort命令的具体执行方式(格式如\"-i\")");
+        String sortDetail = sc.nextLine();
+        System.out.println("请输入grep命令的具体执行方式及匹配内容(格式如\"-h abc\")");
+        String grepDetail = sc.nextLine();
+        System.out.println("请输入cut命令的具体执行方式及位数(格式如\"-b 3\")");
+        String cutDetail = sc.nextLine();
+        System.out.println("请输入wc命令的具体执行方式(格式如\"-c\")");
+        String wcDetail = sc.nextLine();
+
+
         try {
             process = Runtime.getRuntime().exec(cmds);
             BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StringBuffer sb = new StringBuffer();
 
-            String fileName = " LinuxChannel.iml";
+
+
+            StringBuffer sb = new StringBuffer();
             String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
@@ -28,4 +45,24 @@ public class JavaToLinux {
         }
 
     }
+
+    private static String catSortExec(String fileName , String sortDetail) {
+
+        return "cat " + fileName + " | " + "sort " + sortDetail;
+
+    }
+
+    private static String catGrepExec(String fileName , String grepDetail) {
+
+        return "cat " + fileName + " | " + "grep " + grepDetail;
+    }
+
+    private static String catCutExec(String fileName , String cutDetail) {
+        return "cat " + fileName + " | " + "cut " + cutDetail;
+    }
+
+    private static String catWcExec(String fileName , String wcDetail) {
+        return "cat " + fileName + " | " + "cut " + wcDetail;
+    }
+
 }
