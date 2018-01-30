bb5eb0955a1114e452783fa1ed2881e4e37ad2cd
舞枫 <kingwufeng@wufengdeMacBook-Pro.local>
2018-01-30 18:39:38 +0800

    2

---
+++ src/JavaToLinux.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class JavaToLinux {
+
+    public static void main(String[] args) {
+        Process process = null;
+        String[] cmds = {"/bin/bash", "-c", "cat LinuxChannel.iml"};
+        //Scanner sc = null;
+        try {
+            process = Runtime.getRuntime().exec(cmds);
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+
+            String fileName = " LinuxChannel.iml";
+            String line ;
+            while ((line = br.readLine())!= null) {
+                sb.append(line).append("\n");
+            }
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
+
+    }
+}
